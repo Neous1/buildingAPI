68bb2f9df350a01d79a40d824abcee2f92ddd115
Neous1 <Neous1@users.noreply.github.com>
2017-07-19 16:45:40 -0400

    GET request

--- server.js
+++ server.js
@@ -9,5 +9,14 @@ var server = app.listen(3000, listening);
 function listening(){
     console.log("listening...");
 }
-
+//middleware to connect client folder to server
 app.use(express.static("website"));
+
+// interaction
+//Get request
+
+app.get('/flower', sendFlower);
+
+function sendFlower(request, response){
+    response.send("I love flowers too");
+}
