d3fd4f882dd65d0e03fb7554d68d8e5c958e928c
Neous1 <Neous1@users.noreply.github.com>
2017-07-19 16:29:06 -0400

    add call back function to server

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ var express = require("express");
 
 var app= express();
 
-var server = app.listen(3000);
+var server = app.listen(3000, listening);
+
+function listening(){
+    console.log("listening...");
+}
