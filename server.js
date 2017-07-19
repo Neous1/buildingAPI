d0246e704da54b9d80d4727c9417da36548e14b3
Neous1 <Neous1@users.noreply.github.com>
2017-07-19 19:34:23 -0400

    query for search

--- server.js
+++ server.js
@@ -20,5 +20,5 @@ app.get('/search/:flower', sendFlower);// "colon" indicates that "search" is the
 
 function sendFlower(request, response){
     var data = request.params;
-    response.send("I love " + data.flower + " too");
+    response.send("I love " + data.flower + " too"); // flower is what the user entered to search per the app get param
 }
