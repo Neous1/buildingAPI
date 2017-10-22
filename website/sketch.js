486cf70bf8c3408adaa17a017ce184e0bc94f501
Neous1 <Neous1@users.noreply.github.com>
2017-10-21 23:57:07 -0400

    add p5 libraries to create a canvas to display json file.

--- website/sketch.js
+++ website/sketch.js
@@ -1,7 +1,7 @@
 function setup() {
   // put setup code here
   createCanvas (400, 400);
-  background(51, [a])
+  background(51)
   loadJSON("all", gotData);
   console.log("running");
 }
@@ -10,12 +10,13 @@ function gotData(data) {
   console.log(data);
   var keys = Object.keys(data);
   for(var i = 0; i< keys.length; i++){
-    var word = key[i];
+    var word = keys[i];
     var score = data[word];
     var x = random(width);
     var y = random(height);
     fill(255);
     textSize(64)
+    text(word, x, y);
   }
 
 }
