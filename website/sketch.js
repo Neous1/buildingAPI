14e653d307bd5576ee75b4bd48465bfab2fa92db
Neous1 <Neous1@users.noreply.github.com>
2017-10-22 10:16:35 -0400

    added  mousePressed event t submit button add  submitWord function to capture word and score from user draw background on refresh clear background on refresh

--- website/sketch.js
+++ website/sketch.js
@@ -1,12 +1,33 @@
 function setup() {
   // put setup code here
-  createCanvas (400, 400);
-  background(51)
-  loadJSON("all", gotData);
+  createCanvas (300, 300);
+  drawData();
   console.log("running");
+  var button = select ("#submit");
+  button.mousePressed(submitWord);
+
 }
 
-function gotData(data) {
+function drawData(){
+  loadJSON("all", gotData);
+  
+}
+
+function submitWord(){
+  var word = select("#word").value();
+  var score = select("#score").value();
+  console.log(word, score);
+  //create route in code tocapture user's input
+  loadJSON("add/" +word+"/"+score, finished);
+  function finished(data){
+    console.log(data);
+    drawData();
+  }
+
+}
+
+function gotData(data) { 
+  background(51);
   console.log(data);
   var keys = Object.keys(data);
   for(var i = 0; i< keys.length; i++){
@@ -15,7 +36,7 @@ function gotData(data) {
     var x = random(width);
     var y = random(height);
     fill(255);
-    textSize(64)
+    textSize(18)
     text(word, x, y);
   }
 
